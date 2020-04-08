2ae8745d528677eefbaa8424d96fa9c17b85f427
agusgutierrez <agusgutierrezperez@gmail.com>
2020-04-08 08:24:37 +0200

    randomLetters

--- main.js
+++ main.js
@@ -1,122 +1,134 @@
-class Sticker{
-    constructor(name,image){
-        this.name = name
-        this.image = image
-        this.points = 0
+let stickers = [
+    {name: "Banana", image : "/assets/img/banana.jpg"},
+    {name: "Bear", image : "/assets/img/bear.jpg"},
+    {name: "Bee", image : "/assets/img/bee.jpg"},
+    {name: "Car", image : "/assets/img/car.jpg"},
+    {name: "Carrot", image : "/assets/img/carrot.jpg"},
+    {name: "Cow", image : "/assets/img/cow.jpg"},
+    {name: "Dog", image : "/assets/img/cat.jpg"},
+    {name: "Duck", image : "/assets/img/duck.jpg"},
+    {name: "Donut", image : "/assets/img/donut.jpg"},
+    {name: "Pig", image : "/assets/img/pig.jpg"},
+    {name: "Plane", image : "/assets/img/plane.jpg"},
+    {name: "Pizza", image : "/assets/img/pizza.jpg"}
+]
+let letters = ["B","C","D","P"]
+
+  function shuffle(a) {
+    var j, x, i;
+    for (i = a.length - 1; i > 0; i--) {
+        j = Math.floor(Math.random() * (i + 1));
+        x = a[i];
+        a[i] = a[j];
+        a[j] = x;
     }
-    party(){
+    return a;
+}
+
+   function displayImages(letter){
+       switch (letter) {
+           case "C":
+               document.getElementById("ce").classList.add("animated","infinite","bounce")
+               break;
+               case "D":
+                document.getElementById("de").classList.add("animated","infinite","bounce")
+                break;
+                case "B":
+                document.getElementById("be").classList.add("animated","infinite","bounce")
+                break;
+           default:
+            document.getElementById("pe").classList.add("animated","infinite","bounce")
+               break;
+       }
+    let currentLetter = letter
+    const scorePlace = document.querySelector(".score span")
+    const chances = document.querySelector(".chances")
+    const optimism = document.querySelector(".optimism")
+    let total = 100
+    let wrong = 4
+    let right = []
+    scorePlace.innerHTML = total 
+    let arrayShuffled = shuffle(stickers)
+       for (const key in arrayShuffled) {
+       if (arrayShuffled.hasOwnProperty(key)) {
+           const element = arrayShuffled[key];
+           if(element.name[0] !== currentLetter){
+            var image = new Image(150,150)
+            image.src = element.image
+            const lugar = document.querySelector(".car")
+            const newImage = lugar.appendChild(image)
+            newImage.addEventListener('click', event => {
+            newImage.classList.add('animated', 'wobble')
+            wrong -= 1
+            chances.classList.remove("chances")
+            chances.classList.add("chancesDisplay","animated","bounceOutUp")
+            chances.innerHTML = "You have " + wrong +" more tries"
+            if(wrong <= 1){
+                chances.innerHTML = "This is your last chance!"
+            }
+            gameHope()
+            if(wrong === 0){
+               loseMessage()
+               gameReload()
+            }
+        })
+        } else {
+            var image = new Image(150,150)
+            image.src = element.image
+            const lugar = document.querySelector(".car")
+            const newImage = lugar.appendChild(image)
+            newImage.addEventListener('click', event => {
+            newImage.classList.add('animated', 'fadeOutUp')
+            right.push(element)
+            optimism.classList.remove("optimism")
+            optimism.classList.add("optimismDisplay","animated","bounceOutUp")
+            gameGreetings()
+            if(right.length === 3){
+                party()
+                gameReload()
+            } 
+            });
+        }
+       }
+   }
+    }
+    function party(){
         let node = document.querySelector(".loser");
         node.classList.remove("loser")
         node.classList.add("winner","animated","infinite","swing")
         let background = document.querySelector(".displayGame")
         background.classList.add("loser")
     }
-    gameReload(){
+    function gameReload(){
         let timeoutID
         timeoutID = window.setTimeout(reload, 2000);
         function reload(){
             location.reload()
         }
     }
-    loseMessage(){
+    function loseMessage(){
         let node = document.querySelector(".helper");
         node.classList.remove("helper")
         node.classList.add("help")
         }
-       
-    }
-
-class Letter extends Sticker{
-    constructor(name){
-        super(name)
-    }
-}
-
-
-class Game {
-    constructor(){
-        this.stickers = [];
-        this.letters = [];
-        this.score = 100;
-    }
-     addSticker(sticker){
-         this.stickers.push(sticker)
-     }
-     addLetter(letter){
-        this.letters.push(letter)
-    }
-    shuffle(a) {
-        var j, x, i;
-        for (i = a.length - 1; i > 0; i--) {
-            j = Math.floor(Math.random() * (i + 1));
-            x = a[i];
-            a[i] = a[j];
-            a[j] = x;
-        }
-        return a;
-    }
-    getRandomLetter(){
-        return this.letters[ Math.floor(Math.random() * this.letters.length)]
-    }
-    displayImages(){
-        let currentLetter = this.displayLetter()
-        const scorePlace = document.querySelector(".score span")
-        const chances = document.querySelector(".chances span")
-        console.log(chances)
-        let total = this.score
-        let wrong = 3
-        let right = []
-        scorePlace.innerHTML = total 
-        this.shuffle(this.stickers).forEach(function(element){
-            if(element.name[0] !== currentLetter){
-                var image = new Image(150,150)
-                image.src = element.image
-                const lugar = document.querySelector(".car")
-                const newImage = lugar.appendChild(image)
-                newImage.addEventListener('click', event => {
-                newImage.classList.add('animated', 'wobble')
-                wrong -= 1
-                chances.innerHTML = "You have " + wrong +" more tries"
-                if(wrong <= 1){
-                    chances.innerHTML = "This is your last chance!"
-                }
-                if(wrong === 0){
-                    element.loseMessage()
-                    element.gameReload()
-                }
-            })
-            } else {
-                var image = new Image(150,150)
-                image.src = element.image
-                const lugar = document.querySelector(".car")
-                const newImage = lugar.appendChild(image)
-                newImage.addEventListener('click', event => {
-                newImage.classList.add('animated', 'fadeOutUp')
-                right.push(element)
-                chances.innerHTML = "Fantastic!!"
-                if(right.length === 3){
-                    element.party()
-                    element.gameReload()
-                } 
-                });
+     function gameGreetings(){
+        let timeoutID
+        timeoutID = window.setTimeout(show,1000)
+        function show(){
+        const optimism = document.querySelector(".optimismDisplay")
+        optimism.classList.remove("optimismDisplay")
+        optimism.classList.add("optimism")
             }
-            })
+            }
+        function gameHope(){
+         let timeoutID
+         timeoutID = window.setTimeout(show,1000)
+         function show(){
+         const optimism = document.querySelector(".chancesDisplay")
+         optimism.classList.remove("chancesDisplay")
+         optimism.classList.add("chances")
+         }   
         }
-       
-        displayLetter(){
-            let letter = document.querySelector(".letter h1")
-            let randomLetter = this.getRandomLetter()
-            let text = randomLetter.name
-            letter.innerHTML= text
-            return randomLetter.name
-        }
-        displayScore(){
-        
-        }
-    }
-    
-     
-   
-  
     // .classList.add('animated', 'wobble') 
-
+let letterCurrent = shuffle(letters).pop()
+displayImages(letterCurrent)
